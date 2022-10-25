f5c3bee3612e48cf6ac92ce8504a8c3cecb5b0cd
Austin <pizzahi5@gmail.com>
2022-10-24 20:10:59 -0700

    Added_Index_Price_Contract

---
+++ contracts/fortunity modifications/FortunityIndexPrice.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.7.6;
+
+import { PerpMath } from "./lib/PerpMath.sol";
+//truflation import
+
+contract FortunityIndexPrice {
+
+    uint256 private _priceIndex;
+
+    // External Non-View
+
+    function initialize(
+
+    ) external initializer {
+
+    }
+
+    // External View
+
+    function getPriceIndex () external view returns (uint256) {
+        return _priceIndex;
+    } 
+}
